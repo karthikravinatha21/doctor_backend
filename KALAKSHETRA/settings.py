1b02d5521e0d2321d39b881159c072ec9de32298
vaidyabandhu <support@vaidyabandhu.com>
2025-09-06 23:49:29 +0530

    update

--- KALAKSHETRA/settings.py
+++ KALAKSHETRA/settings.py
@@ -178,11 +178,11 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test',
-        'USER': 'healthcare',
-        'PASSWORD': 'Health@Admin@24',
-        'HOST': '35.207.201.1',
-        'PORT': '5462',
+        'NAME': 'Vaidya',
+        'USER': 'postgres',
+        'PASSWORD': 'Vaidya@Bandhu@25',
+        'HOST': '52.66.199.115',
+        'PORT': '5432',
     }
 }
 
