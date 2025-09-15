e6b9e7cf464d3c9ad2be6b04c7d8dcb6a92bf979
Nitin Faujdar <nitinfaujdarnfs@gmail.com>
2025-09-15 22:51:03 +0530

    doctor list added in hospital

--- apps/hospital/serializers.py
+++ apps/hospital/serializers.py
@@ -1,12 +1,33 @@
 from rest_framework import serializers
 
 from apps.hospital.models import Hospital
+from apps.doctors.models import Doctor
+from apps.master_data.serializers import SpecialisationSpecificSerializer
+
+class DoctorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Doctor
+        exclude = ('updated_at','created_at', 'password', 'hospital')
+
+    def to_representation(self, instance):
+        response_object = super().to_representation(instance)
+        if instance.speciality:
+            response_object['speciality'] = SpecialisationSpecificSerializer(instance.speciality.all(), many=True).data
+        return response_object
 
 
 class HospitalSerializer(serializers.ModelSerializer):
+    doctors = serializers.SerializerMethodField('get_doctors')
+
     class Meta:
         model = Hospital
         exclude = ('created_at', 'updated_at',)
+    
+    def get_doctors(self, instance):
+        obj = Doctor.objects.filter(hospital=instance)
+        serializer = DoctorSerializer(obj, many=True)
+        return serializer.data
 
     def to_representation(self, instance):
         response_object = super().to_representation(instance)
--- apps/hospital/views.py
+++ apps/hospital/views.py
@@ -18,7 +18,7 @@ class HospitalViewSet(custom_viewsets.ModelViewSet):
     status_code = 200
     # Add this line
     filter_backends = [filters.SearchFilter]
-    search_fields = ['hospital_name', 'city__city_name']
+    search_fields = ['name', 'city__city_name']
 
     def get_permissions(self):
         """
