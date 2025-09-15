ca4c88d9fcdd5c19b35873b01f9220fce96b7da1
Nitin Faujdar <nitinfaujdarnfs@gmail.com>
2025-09-14 22:51:04 -0700

    settings configurations

--- KALAKSHETRA/settings.py
+++ KALAKSHETRA/settings.py
@@ -47,11 +47,16 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
-    "*",
+    'https://6c71-2401-4900-1cc5-19db-28ec-9069-cee9-5a24.ngrok-free.app',
+    "https://stage.vaidyabandhu.com",
+    "https://www.vaidyabandhu.com",
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "*",
+    "http://localhost:3000",  # React dev server
+    "https://6c71-2401-4900-1cc5-19db-28ec-9069-cee9-5a24.ngrok-free.app",
+    "https://stage.vaidyabandhu.com",
+    "https://www.vaidyabandhu.com",
 ]
 
 # Read .env.development file if it exists
@@ -168,12 +173,13 @@ DATABASES = {
     # }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'vb_local',
+    #     'NAME': 'postgres',
     #     'USER': 'postgres',
-    #     'PASSWORD': 'postgres',
+    #     'PASSWORD': 'Fogus_1234',
     #     'HOST': 'localhost',
     #     'PORT': '5432',
     # }
+
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'vaidya',
