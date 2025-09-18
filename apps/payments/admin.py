a9f50c37912f158a26cfcbc9fdcf2299df43ec64
Nitin Faujdar <nitinfaujdarnfs@gmail.com>
2025-09-18 09:01:44 +0530

    sub department fixes

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -14,17 +14,7 @@ class AdminPaymentTransactions(admin.ModelAdmin):
     # search_fields = ('actor__first_name', 'movie__title', 'role')
     # autocomplete_fields = ('actor', 'movie')
 
-class AdminSubscriptionForm(forms.ModelForm):
-    class Meta:
-        model = Subscription
-        fields = '__all__'
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['sub_department'].queryset = SubDepartment.objects.all().only('name')
-
 @admin.register(Subscription)
 class AdminSubscription(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'currency')
     list_filter = ('id', 'name', 'price', 'currency')
-    filter_horizontal = ('sub_department',)
--- apps/payments/models.py
+++ apps/payments/models.py
@@ -18,7 +18,7 @@ class Subscription(models.Model):
     currency = models.CharField(max_length=10, default='INR')
     duration = models.CharField(max_length=10, choices=duration_choices)
     discount = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    sub_department = models.ManyToManyField(SubDepartment, related_name="subscription_subscription_master")
+    # sub_department = models.ManyToManyField(SubDepartment, related_name="subscription_subscription_master")
 
     def __str__(self):
         return f"{self.name} - {self.price} {self.currency} ({self.duration})"
