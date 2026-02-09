b0216f91840423ee9c47e51886cd82d48615abc5
sathish <sathish@edunxt.ai>
2026-02-09 23:48:35 +0530

    corrupted

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -7,6 +7,7 @@ from apps.payments.models import UserSubscription
 from user_details.models import User, FamilyMember
 
 class UserDataSerializer(serializers.ModelSerializer):
+    membership_id = serializers.CharField(read_only=True, required=False, allow_blank=True, allow_null=True)
     start_date = serializers.SerializerMethodField('get_start_date')
     end_date = serializers.SerializerMethodField('get_end_date')
     is_active = serializers.SerializerMethodField('get_is_active')
